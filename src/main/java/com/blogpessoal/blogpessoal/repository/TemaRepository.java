967844991a3d38f4e9a946b10c13c0ca545410ac
˜Edlane <edlane.santana@mercadolivre.com>
2023-02-01 11:56:25 -0300

    Adicionando a classe Tema e realizando os relacionamentos

--- src/main/java/com/blogpessoal/blogpessoal/repository/TemaRepository.java
+++ src/main/java/com/blogpessoal/blogpessoal/repository/TemaRepository.java
@@ -3,9 +3,12 @@ package com.blogpessoal.blogpessoal.repository;
 import com.blogpessoal.blogpessoal.model.Tema;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 import java.util.List;
 
+@Repository
 public interface TemaRepository extends JpaRepository<Tema, Long> {
 
   public List<Tema> findAllByDescricaoContainingIgnoreCase(@Param("descricao") String descricao);
